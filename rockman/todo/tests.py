276377e2a5bc6510c07e64e97721bb353a7caa14
hayne <ebuschang@gmail.com>
2015-02-20 16:00:37 -0600

    added tox, created first test in todo app

--- rockman/todo/tests.py
+++ rockman/todo/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from rockman.todo.models import Todo
+from datetime import datetime
 
-# Create your tests here.
+class TodoTestCase(TestCase):
+    def setUp(self):
+        due_date = datetime.now()
+        Todo.objects.create(task="Make test.", category="House", assignee="Emily", due=due_date)
+
+    def test_return_tasks(self):
+        """Play test case for place holding to get testing up and running"""
+        todo_task = Todo.objects.get(assignee="Emily")
+        self.assertEqual(todo_task.task, 'Make test.')
