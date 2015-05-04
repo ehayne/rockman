82068b1aba379ba0db51b52450c76c0a3718283a
hayne <ebuschang@gmail.com>
2015-05-04 16:20:26 -0500

    fixed up small page details

--- rockman/processor.py
+++ rockman/processor.py
@@ -12,11 +12,8 @@ def default(request):
                     'base/js/jquery-1.11.2.min.js',
                     # 'base/js/tabs.js',
                     ],
-        'js_file_url': [
-                        # 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular.min.js',
-                        # 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
-                    ],
-        'page_title': 'Emily & Kyle rockman.life',
+
+        'page_title': 'emily & kyle rockman.life',
         'window_title': 'rockman.life',  #use full names for search engine optimization
-        'footer_email': 'buschang.rockman.wedding@gmail.com',
+        'footer_email': 'support@rockman.life',
     }
