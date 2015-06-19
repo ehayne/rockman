67d353751b6af0f1a52b43b2de170905f9392e73
Kyle Rockman <kyle.rockman@mac.com>
2015-06-19 17:31:28 -0500

    fix deploy script
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

---
+++ jenkins/deploy.sh
@@ -0,0 +1,24 @@
+echo $WORKSPACE
+export PROJECT_NAME=rockman
+export ROOT="/usr/local/$PROJECT_NAME"
+export VENV_DIR="$ROOT/venv"
+export SUPERVISOR_CONF="/usr/local/supervisor/${PROJECT_NAME}.conf"
+export NGINX_SRC_CONF="/etc/nginx/site-available/${PROJECT_NAME}.conf"
+export NGINX_ENABLED_CONF="/etc/nginx/site-enabled/${PROJECT_NAME}.conf"
+
+if [[ -e "$VENV_DIR/bin/activate" ]]
+then
+  rm -rvf "$VENV_DIR"
+  rm ~/.rockman/rockman.conf.py
+fi
+virtualenv "$VENV_DIR"
+. "$VENV_DIR/bin/activate"
+pip install -r "./requirements.txt"
+python setup.py install
+
+cp -rv "./jenkins/nginx.conf" "${SUPERVISOR_CONF}"
+cp -rv "./jenkins/nginx.conf" "${NGINX_SRC_CONF}"
+ln -snf "${NGINX_SRC_CONF}" "${NGINX_ENABLED_CONF}"
+
+rockman init
+rockman migrate
